a3d4f312d0404e84b006f334e3e746d0dc1d4ae6
jen10bart <98769849+jen10bart@users.noreply.github.com>
2022-01-31 13:11:15 -0500

    Add files via upload

---
+++ lab04_turtle.py
@@ -0,0 +1,16 @@
+import turtle
+wn=turtle.Screen()
+ed=turtle.Turtle()
+
+ed.speed('fastest')
+ed.pencolor('green')
+square=int(input("How many squares to draw?: "))
+units=int(input("What unit to increment the length of each side of the square?: "))
+
+
+for i in range(square*4):
+    ed.forward(10)
+    ed.right(90)
+    units=10+units
+    ed.forward(units)
+   
